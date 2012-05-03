76e3a521a0e6d3a813c9c72f674b09647e9baf8a
Orlov Nikita <nukich74@mail.ru>
2012-05-04 00:39:57 +0400

    It works! Need to add more new and delete operators. So, I hava no idea what iteresting to add.

--- GarbageCollector/main.cpp
+++ GarbageCollector/main.cpp
@@ -1,9 +1,25 @@
+#include <utility>
+#include <map>
+#include <cstdio>
+#include <algorithm>
 #include <iostream>
 #include <cstdio>
 #include <fstream>
 #include "object.h"
 
 
+
+class A : public Object {
+private:
+	int Value;
+public:
+	A(const int &a): Object(),  Value(a) { }
+};
+
 int main() {
+	int c = 100;
+	A q(c);
+	A* ptr = new A(q);
+	CollectAllGarbage();
 	return 0;
 }
--- GarbageCollector/object.h
+++ GarbageCollector/object.h
@@ -1,24 +1,42 @@
 #ifndef _OBJECT_INHARITANCE_ 
 #define _OBJECT_INHARITANCE_ 1
+#include <utility>
+#include <map>
+#include <cstdio>
+#include <algorithm>
 
-namespace GarbageCollect {
-	enum AchieveType { };	
-	std::map< std::string, void* > ObjectMap;
+namespace GarbageCollector {
+		enum SpaceType { Heap, Stack };	
+	std::map< void*, SpaceType > ObjectMap;
 	
 	class Object {
 	private:	
-	
+
 	public:
+		Object() {
+			ObjectMap.insert(std::make_pair(this, Stack));
+		}
 		
-		void *operator new(const size_t &size) {
-			
+		void *operator new(size_t size) {
+			void* ptr = malloc(size); 
+			ObjectMap.insert(std::make_pair(ptr, Heap));
+			return ptr;
 		}
 		
 		void operator delete(void* ptr) {
-			free(p);
+			free(ptr);
 		}
 	
 		virtual ~Object() { }	
 	};
+	
+	void CollectAllGarbage() {
+		for (auto s = ObjectMap.begin(); s != ObjectMap.end(); ++s) {
+			if (s->second != Stack) {
+		/*DEBUT OUTPUT*/		std::cout << "Deleted " << s->second;
+				delete (Object*)s->first;
+			} 
+		}
+	}
 }
 #endif
